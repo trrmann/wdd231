865df835267d1dd4ac0ac614b6292dbd528a3dea
trrmann <113477307+trrmann@users.noreply.github.com>
2025-11-03 15:27:19 -0500

    fix js to read as the json was built

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -8,7 +8,7 @@ async function fetchDirectoryData(container) {
             throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        displayDirectory(data.businesses, container);
+        displayDirectory(data.members, container);
     } catch (error) {
         console.error('There has been a problem with your fetch operation:', error);
     }
