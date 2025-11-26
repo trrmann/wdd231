8102151560de7c7575a56f705af437a14c34fb05
trrmann <113477307+trrmann@users.noreply.github.com>
2025-11-26 11:26:54 -0500

    added discover page, added supporting files for the new page and updated the links from all other pages to point to the new discover page.

---
+++ chamber/modules/discover.mjs
@@ -0,0 +1,6 @@
+export async function DisplayDiscoverInformation(discoverMainContainerClass) {
+    const discoverMainContainer = document.querySelector(discoverMainContainerClass);
+
+    discoverMainContainer.textContent = 'discover';
+
+}
