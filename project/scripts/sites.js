9c625aed1c66c8f63c989ab60f0a5178fd5180a0
trrmann <113477307+trrmann@users.noreply.github.com>
2025-11-28 12:30:45 -0500

    files structure added for project based on site plan

---
+++ project/scripts/sites.js
@@ -0,0 +1,70 @@
+import { SetCopyWriteDate, SetLastModifiedDate } from "../modules/date.mjs";
+import { RegisterNavButton } from "../modules/navigation.mjs";
+import { RegisterDarkModeButton } from "../modules/preference.mjs";
+import { DisplaySitesInformation } from "../modules/sites.mjs";
+
+const darkModeElementConfiguration = {
+    classList: [
+        '.body',
+        '.header',
+        '#drk-btn',
+        '.darkModeImage',
+        '.hamburger',
+        '.nav-itm',
+        '.nav-itm.current',
+        '.nav-lnk',
+        '.site-plan-main',
+        '.desktopWireframesSectionData',
+        '.mobileWireframesSectionData',
+        '.footer',
+        '.facebook-icon',
+        '.instagram-icon',
+        '.twitter-icon',
+        '.linked-in-icon',
+        '.site-plan-link',
+        '.class-home-link'
+    ],
+    images: [
+        {
+            class: '.darkModeImage',
+            lightImage: 'images/dark-mode.svg',
+            darkImage:'images/light-mode.svg',
+            height:'44',
+            width:'44'
+        },
+        {
+            class: '.facebook-icon',
+            lightImage: 'images/facebook.svg',
+            darkImage:'images/light-facebook.svg',
+            height:'50',
+            width:'50'
+        },
+        {
+            class: '.instagram-icon',
+            lightImage: 'images/instagram.svg',
+            darkImage:'images/light-instagram.svg',
+            height:'50',
+            width:'50'
+        },
+        {
+            class: '.twitter-icon',
+            lightImage: 'images/twitter.svg',
+            darkImage:'images/light-twitter.svg',
+            height:'50',
+            width:'50'
+        },
+        {
+            class: '.linked-in-icon',
+            lightImage: 'images/linkedin.svg',
+            darkImage:'images/light-linkedin.svg',
+            height:'50',
+            width:'50'
+        }
+    ]
+}
+
+RegisterDarkModeButton('#drk-btn', darkModeElementConfiguration);
+RegisterNavButton('#ham-btn','#nav-bar',['.nav-lnk']);
+SetCopyWriteDate('.currentyear');
+SetLastModifiedDate('.lastModified');
+DisplaySitesInformation('.sites-main');
